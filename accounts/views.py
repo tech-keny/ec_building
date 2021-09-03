832429b615f31a2009a1c94f4fbe3d026dab2cab
tech-keny <chanken7@icloud.com>
2021-09-03 21:41:17 +0900

    9/3更新

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.views import View
+from django.shortcuts import render, redirect
+
+
+class ProfileView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'accounts/profile.html') 
